0bbf00f4a78b0b77414f85011fb15627b3018f03
giovana <giovananogueira2006@gmail.com>
2024-06-28 12:21:02 -0300

    Aula 04

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,5 +1,10 @@
 def fibonacci(qtt:int):
     sequence = [0,1]
 
-    return sequence
+    i = 1 
 
+    while i < qtt:
+        sequence.append(sequence[i-1] + sequence[i-2])
+        i += 1
+
+    return sequence
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,6 +1,13 @@
 from scripts.fibonacci import fibonacci
 
+def test_first_2_numbers():
+    res = fibonacci(2)
+    assert res == [0,1]
+
+def test_first_5_numbers():
+    res = fibonacci(5)
+    assert res == [0,1,1,2,3]
+
 def test_first_10_numbers():
     res = fibonacci(10)
-    assert res == [0,1,1,2,3,5,8,13,21,34,]
-
+    assert res == [0,1,1,2,3,5,8,13,21,34]
